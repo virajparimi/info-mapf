d85b994964e437e6a0bc04be64981580aebf84a0
Viraj Parimi <parimiviraj@gmail.com>
2024-01-09 22:48:43 -0500

    added base map code along with some GP code

--- src/map.py
+++ src/map.py
@@ -5,26 +5,106 @@ from dataclasses import dataclass
 @dataclass
 class Observation:
     location: float  # l
-    observation: float  # y
+    measurement: float  # y
 
 
 class Map(object):
-    def __init__(self, maze, start):
-        self.map = maze
-        self.start = start
-        self.height, self.width = maze.shape[0], maze.shape[1]
+    def __init__(self, maze, means=None, locations=None):
+        self.map = (
+            maze  # Should be a boolean array indicating where the agents/obstacles are
+        )
+        self.num_of_rows, self.num_of_cols, self.map_size = (
+            maze.shape[0],
+            maze.shape[1],
+            maze.shape[0] * maze.shape[1],
+        )
 
-        # Map's decay rate
-        self.theta = 0.01
+        if means is None:
+            self.means: list = [1]
+        else:
+            self.means: list = means
 
-        # Uniform measurement prior - p(u_i) - set to 0.5
-        self.feature_priors = np.ones((self.height, self.width)) * 0.5
+        if locations is None:
+            self.locations: list = [
+                self.linearize_coordinate(self.num_of_rows // 2, self.num_of_cols // 2)
+            ]
+        else:
+            self.locations: list = []
+            for loc in locations:
+                self.locations.append(self.linearize_coordinate(loc[0], loc[1]))
 
-        # Noisy mesurement prior - p(y_i | u_i) - set to 0.1 i.e 10% chance of being wrong
-        self.measurement_noise_priors = np.ones((self.height, self.width)) * 0.1
+        self.location_means = np.zeros(self.map_size)
+        for location_id, mean in zip(self.locations, self.means):
+            self.location_means[location_id] = mean
 
-        # Phenomenons - p(x_i)
-        self.phenomenon_likelihoods = np.zeros((self.height, self.width))
+        self.theta_1 = 0.4  # variance of isolated feature
+        self.theta_2 = 0.01  # characteristic length for covariance decay
+
+        self.measurement_noise = 0.2
+
+    def get_row_coordinate(self, location_id):
+        return location_id // self.num_of_cols
+
+    def get_column_coordinate(self, location_id):
+        return location_id % self.num_of_cols
+
+    def get_coordinate(self, location_id):
+        return np.array(
+            [
+                self.get_row_coordinate(location_id),
+                self.get_column_coordinate(location_id),
+            ]
+        )
+
+    def linearize_coordinate(self, row: int, column: int) -> int:
+        return self.num_of_cols * row + column
+
+    def get_manhattan_distance(self, location_id_a, location_id_b):
+        location_a = self.get_coordinate(location_id_a)
+        location_b = self.get_coordinate(location_id_b)
+        return abs(location_a[0] - location_b[0]) + abs(location_a[1] - location_b[1])
+
+    def valid_move(self, current, next):
+        if next < 0 or next >= self.map_size or self.map[next]:
+            return False
+        return self.get_manhattan_distance(current, next) < 2
+
+    def get_neighbors(self, current):
+        neighbors = []
+        candidates = [
+            current + 1,
+            current - 1,
+            current + self.num_of_cols,
+            current - self.num_of_cols,
+        ]
+        for next in candidates:
+            if self.valid_move(current, next):
+                neighbors.append(next)
+        return neighbors
+
+    # Defines the mean function "m" for the Gaussian Process
+    def mean_function(self, location_ids):
+        means = np.zeros(len(location_ids))
+        for idx, location_id in enumerate(location_ids):
+            means[idx] = self.location_means[location_id]
+        return means
+
+    def covariance_function(self, location_id_a, location_id_b):
+        assert len(location_id_a) == 1
+        location_a = self.get_coordinate(location_id_a)
+        location_b = self.get_coordinate(location_id_b)
+        distance = float(np.linalg.norm(location_a - location_b))
+        covariance = self.theta_1 * np.exp(-distance / (self.theta_2**2))
+        return covariance
+
+    def kernel_function(self, location_ids_a, location_ids_b):
+        kernel_matrix = np.zeros((len(location_ids_a), len(location_ids_b)))
+        for idx_a, location_id_a in enumerate(location_ids_a):
+            for idx_b, location_id_b in enumerate(location_ids_b):
+                kernel_matrix[idx_a, idx_b] = self.covariance_function(
+                    location_id_a, location_id_b
+                )
+        return kernel_matrix
 
     def get_feature_prior(self, location):
         # p(u_i)
--- src/mdp.py
+++ src/mdp.py
@@ -5,26 +5,57 @@ class MarkovDecisionProcess(object):
     def __init__(self, start, map):
         observation = map.get_observation(
             start
-        )  # discrete observation for now ->  should be a tuple of (location, observation)
+        )  # continuous observation for now ->  should be a tuple of (location, measurement)
         self.observations = [observation]
         self.init_state = [
             self.observations,
         ]
         self.actions = ["Left", "Right", "Up", "Down"]
 
-    def compute_conditional_measurement(self, location, map):
-        feature_prior = map.get_feature_prior(location)  # p(u_i)
+    # Should return p(y_i | y^{0:t})
+    def compute_conditional_measurement(self, location_ids, map):
+        # m(l) and v(l, l)
+        mean_of_location = map.mean_function(location_ids)
+        covariance_of_location = map.kernel_function(location_ids, location_ids)
 
-        # Equation 4.24
-        feature_likelihood = 0  # p(u_i | y_{0:t})
-        for observation in self.observations:  # k = 0 .. t
-            local_feature_likelihood = 1.0
-            for local_feature_prior in map.get_feature_prior(observation.location):
-                local_distance = float(np.linalg.norm(location - observation.location))
-                probabilistic_correlation = feature_prior + np.exp(
-                    (-1.0 * local_distance) / (map.theta**2)
-                )
-                local_feature_likelihood += (
-                    map.get_measurement_noise_prior(observation.location)
-                    * local_feature_prior
-                ) / feature_prior
+        # l^{0:t}
+        observation_locations = [
+            observation.location for observation in self.observations
+        ]
+        # y^{0:t}
+        observation_measurements = [
+            observation.measurement for observation in self.observations
+        ]
+
+        # v(l, l^{0:t})
+        covariance_location_observations = map.kernel_function(
+            location_ids, observation_locations
+        )
+
+        # v(l^{0:t}, l)
+        covariance_observations_location = map.kernel_function(
+            observation_locations, location_ids
+        )
+
+        # v(l^{0:t}, l^{0:t})
+        covariance_observations = map.kernel_function(
+            observation_locations, observation_locations
+        )
+
+        mean = mean_of_location + covariance_location_observations @ np.linalg.inv(
+            covariance_observations
+            + np.eye(len(self.observations) * map.measurement_noise)
+        ) @ (
+            np.array(observation_measurements)
+            - map.mean_function(observation_locations)
+        )
+        covariance = (
+            covariance_of_location
+            - covariance_location_observations
+            @ np.linalg.inv(
+                covariance_observations
+                + np.eye(len(self.observations)) * map.measurement_noise
+            )
+            @ covariance_observations_location
+            + np.eye(len(location_ids)) * map.measurement_noise
+        )
